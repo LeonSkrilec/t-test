52c7c56f4a9c1c7f48c8d1be7f2810fb2bc63308
Leon Skrilec <l.skrilec@gmail.com>
2020-12-14 17:44:38 +0100

    Share tags

--- components/Meta.js
+++ components/Meta.js
@@ -6,31 +6,23 @@ export default function Head() {
     <NextHead>
       <title>T-test | Online statistični izračuni</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <meta
-        name="description"
-        content="Online izračuni elementarnih statističnih operacij."
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="152x152"
-        href="/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-      />
+      <meta name="description" content="Online izračuni elementarnih statističnih operacij." />
+      <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       <link rel="manifest" href="/site.webmanifest" />
       <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#1F5FA8" />
       <meta name="msapplication-TileColor" content="#1F5FA8"></meta>
       <meta name="theme-color" content="#ffffff"></meta>
+
+      <meta property="og:url" content="https://t-test-si" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="T-test | Online statistični izračuni" />
+      <meta
+        property="og:description"
+        content="Odprtokodna aplikacija za hitre statistične izračune z jasno interpretacijo rezultatov."
+      />
+      <meta property="og:image" content="https://t-test.si/share.png" />
     </NextHead>
   );
 }
